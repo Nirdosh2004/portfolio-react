0187c269005b8f3afb0c3f6665b04481261c9ebd
Nirdosh2004 <nirdoshkushwaha75@gmail.com>
2024-07-03 23:28:55 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
+import Services from './components/Services/Services'
+import MyWork from './components/MyWork/MyWork'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
  
 const App = () => {
   return (
@@ -9,6 +13,10 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <About/>
+      <Services/>
+      <MyWork/>
+      <Contact/>
+      <Footer/>
     </div>
   )
 }
